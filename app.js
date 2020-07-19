98d879e2049fae0a59221fcad35a0f6a956d3ba9
Htet Lin Maung <jsthtet96@gmail.com>
2020-07-19 07:43:08 +0630

    login register

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const error = require("./middlewares/error");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "running successful" });
 });
+app.use("/api/auth/", require("./routes/AuthRoutes"));
+app.use(error);
 
 mongoose
   .connect(process.env.DB_CONNECTION, {
