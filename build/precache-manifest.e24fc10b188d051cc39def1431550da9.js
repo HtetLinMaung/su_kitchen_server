e9325404510c29e039c8af982616ed28910fc130
Htet Lin Maung <jsthtet96@gmail.com>
2020-08-07 19:34:33 +0630

    deploy react

---
+++ build/precache-manifest.e24fc10b188d051cc39def1431550da9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe7736a885d713ceb92156da47ab850",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3825b1222af16df2d62",
+    "url": "/static/css/main.922f977a.chunk.css"
+  },
+  {
+    "revision": "4643b091bd9b2f6dfa71",
+    "url": "/static/js/2.9c71b000.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9c71b000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3825b1222af16df2d62",
+    "url": "/static/js/main.027a0427.chunk.js"
+  },
+  {
+    "revision": "5881d61d2d8c9a946c9c",
+    "url": "/static/js/runtime-main.cfa3fabc.js"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "beb1a9f7d4742df8abd0b34cb17780a8",
+    "url": "/static/media/ma-la-xiang-guo-10.beb1a9f7.jpg"
+  }
+]);
